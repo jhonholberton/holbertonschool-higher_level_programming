0b133ce18de4d3c1ba20faf503c0cfe57c15b411
jhonholberton <3425@holbertonschool.com>
2022-03-17 22:40:43 -0500

    Write a script that lists all shows contained in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT OUTER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
