e205daa844a183b27ca896ccad10c6ace26213b1
jhonholberton <3425@holbertonschool.com>
2022-04-06 22:58:48 -0500

    Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+module.exports = class Square extends require('./4-rectangle') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
