ab2368200fb1882663687dd8dcbc1772a1593a8a
jhonholberton <3425@holbertonschool.com>
2022-04-06 23:11:20 -0500

    Write a function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a function that returns the reversed version of a list
+exports.esrever = function (list) {
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
+};
