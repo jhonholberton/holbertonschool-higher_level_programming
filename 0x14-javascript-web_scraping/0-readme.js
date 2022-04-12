e14959f22fdc6f439c45477fe3ad8e9eb8e65eb6
jhonholberton <3425@holbertonschool.com>
2022-04-11 21:57:33 -0500

    Write a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
