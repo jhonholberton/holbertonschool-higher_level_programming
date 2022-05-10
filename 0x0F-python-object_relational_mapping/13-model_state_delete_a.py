b58284daf1db5b193f562f9c1234f9edbad4ed9d
jhonholberton <3425@holbertonschool.com>
2022-05-10 16:22:30 -0500

    Write a script that deletes all State objects with a name containing the letter a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that deletes all State objects with a name containing the letter a"""
+"""Write a script that deletes all State objects with
+a name containing the letter a"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
