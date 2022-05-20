6af756efbd3106afadb5138baf74ca425a8a2907
jhonholberton <3425@holbertonschool.com>
2022-05-19 22:42:08 -0500

    that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,7 @@
 """that takes in a URL, sends a request to the URL and displays the
 value of the X-Request-Id variable found in the header of the response"""
 
-from urllib import (request)
+from urllib import request
 from sys import argv
 
 if __name__ == "__main__":
