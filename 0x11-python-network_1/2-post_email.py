89ad879e2df691aad29bc40c49b008ccd12f9ba2
jhonholberton <3425@holbertonschool.com>
2022-05-19 22:40:40 -0500

    script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""script that takes in a URL and an email, sends a POST request to the passed URL with
-the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+"""script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)"""
 
-from urllib import (request, parse)
+from urllib import request, parse
 from sys import argv
 
 if __name__ == "__main__":
